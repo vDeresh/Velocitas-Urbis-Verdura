785f29f8a31186cbe2171ffad528cfd913b422df
vDeresh <igor.deroszewski@gmail.com>
2024-06-08 13:11:27 +0200

    08.06.2024 | 13:11

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-import os
-
-print(os.path.abspath(os.getcwd()))
-
 from src.code.game.racesim import free_simulation_interface #, main_mgr
 from src.code.game.menu import menu_main
 
@@ -11,12 +7,11 @@ from src.code.game.menu import menu_main
 # _racing_class_name = "CAT-B"
 # _race_track_name = "mt5t"
 
-if __name__ == "__main__":
-    # free_simulation_interface(_racing_category_name, _racing_class_name, _race_track_name, main_mgr.ready_drivers(_racing_category_name, _racing_class_name))
-    while 1:
-        output = menu_main()
+# free_simulation_interface(_racing_category_name, _racing_class_name, _race_track_name, main_mgr.ready_drivers(_racing_category_name, _racing_class_name))
+while 1:
+    output = menu_main()
 
-        if output:
-            match output['type']:
-                case "custom-race":
-                    free_simulation_interface(output['category'], output['class'], output['track'], output['drivers'], output['laps'])
+    if output:
+        match output['type']:
+            case "custom-race":
+                free_simulation_interface(output['category'], output['class'], output['track'], output['drivers'], output['laps'])
